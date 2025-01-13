c25ca3c9ec3971328cefb2620a70fe6adf18005c
anonymous <thclark@mit.edu>
2025-01-13 15:06:21 -0500

    prolific mode

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -3,11 +3,11 @@ export default {
   serverUrl: "https://noisy-comp-server-311aa565092d.herokuapp.com",
   socketUrl: "wss://noisy-comp-server-311aa565092d.herokuapp.com/socket",
   // this will be used in prolific mode
-  completionUrl: "https://app.prolific.com/submissions/complete?cc=C5QR6PZ1",
+  completionUrl: "https://app.prolific.com/submissions/complete?cc=C101RMLG",
   contactEmail: "thclark@mit.edu",
   // Either 'debug', 'directLink' or 'prolific'
-  // mode: "prolific",
+  mode: "prolific",
   // mode: "debug",
-  mode: "directLink",
+  // mode: "directLink",
   language: "en",
 };
